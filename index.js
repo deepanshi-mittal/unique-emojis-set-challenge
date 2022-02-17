4c50fd3964e134e4e2bb7058fb2a1bb6d86ef845
Deepanshi Mittal <deepanshimittal1998@gmail.com>
2022-02-17 12:12:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+document.getElementById("btn").addEventListener("click", filterDuplicates)
+
+const emojis = [
+    "💘","💘","🌞","🧡","🍇",
+    "🧁","⚡️","💝","🐱","💘",
+    "🧡","💫","🍓","💜","⚡️",
+    "⚡️","🐶","💘","🍇","🐱",
+    "💘","💫","🍓","💜","💕",
+    "⚡️","💘","🌞","🐱","💘",
+    "💫","🍓","💜","⚡️","🧡",
+    "💕","🍇","💘"
+    ]
+    
+const allEmojisP = document.getElementById("all-emojis-p")
+const uniqueEmojisP = document.getElementById("unique-emojis-p")
+
+for (let emoji of emojis) {
+    allEmojisP.textContent += emoji
+}
+
+function filterDuplicates() {
+    // 1. Filter out all duplicate emojis, leaving only one of each emoji.
+    const result = new Set(emojis)
+    // 2. Render the unique emojis in uniqueEmojisP.
+    for(let emoji of result) {
+        uniqueEmojisP.textContent += emoji
+    }
+    // Just for fun: Replace them with your favourite emojis! 😃 
+}
+
+filterDuplicates()
